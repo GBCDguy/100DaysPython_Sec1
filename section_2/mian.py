661032f5f07b73e0bac0b90df4353ac10be65481
Mr. Coding is EZ <153249227+GBCDguy@users.noreply.github.com>
2024-09-17 17:39:22 +0700

    Add files via upload

---
+++ section_2/mian.py
@@ -0,0 +1,58 @@
+# Subscripting
+print('Hello'[0])
+print('Hello'[-1])
+
+# String
+print('123' + '456')
+
+# Integer and Whole Number
+print(123 + 456)
+
+# Large Integer
+print(123_456_789)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
+
+# Mathematical Operation
+print(123 + 456)
+print(3 - 2)
+print(3 * 2)
+print(4 / 2)
+print(4 // 2)
+print(3 ** 2)
+
+# Order by PEMDAS
+print(3 * (3 + 3) / 3 - 3)
+
+# BMI calculator
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi = weight / height**2
+print(bmi)
+
+
+# Rounding
+bmi = 30.85799449
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+print(round(bmi, 2))
+
+# Assignment Operator
+score = 0
+score += 2
+print(score)
+score -= 1
+print(score)
+score *= 2
+print(score)
+score /= 4
+print(score)
